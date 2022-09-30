a1418e59a10d17ca5cc685177f7d941971767059
JamshedSadullaev <sadullayev98@gmail.com>
2022-09-29 21:58:49 -0400

    deleting some unnecessary things

--- Assets/schema.sql
+++ Assets/schema.sql
@@ -25,31 +25,3 @@ CREATE TABLE employee (
     PRIMARY KEY (id)
 )
 
--- CREATE TABLE departments (
---   id INT NOT NULL AUTO_INCREMENT 
---   PRIMARY KEY,
---   department_name VARCHAR(30)
--- );
-
--- CREATE TABLE roles (
---   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   title VARCHAR(30) NOT NULL,
---   salary DECIMAL NOT NULL,
---   department_id INT,
---   FOREIGN KEY (department_id) 
---   REFERENCES departments(id)
---   ON DELETE SET NULL
--- );
-
--- CREATE TABLE employees (
---   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   first_name VARCHAR(30) NOT NULL,
---   last_name VARCHAR(30) NOT NULL,
---   role_id INT,
---   FOREIGN KEY (role_id)
---   REFERENCES roles(id)
---   ON DELETE SET NULL,
---   manager_id INT,
---   FOREIGN KEY (manager_id) 
---   REFERENCES employees(id)
--- );
